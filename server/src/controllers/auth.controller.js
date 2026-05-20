1fc039d33f3db00eb90576383e2c5d516046e77f
Sachchidanand Gupta <sachchidanandgupta2006@gmail.com>
2026-05-20 14:36:06 +0530

    Implement user authentication with login, get user details, and logout functionality

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 const userModel = require("../models/user.model");
 
 const jwt = require("jsonwebtoken");
+
 async function registerUser(req, res) {
-  const { username, email, passwordHash } = req.body;
+  const { username, email, password } = req.body;
 
   const isUserExists = await userModel.findOne({
     $or: [{ email }, { username }],
@@ -17,11 +18,11 @@ async function registerUser(req, res) {
   const user = await userModel.create({
     username,
     email,
-    passwordHash,
+    passwordHash:password,
   });
   const token = jwt.sign(
     { id: user._id, email: user.email },
-    process.env.JWT_SCERET,
+    process.env.JWT_SECRET,
     { expiresIn: "1d" },
   );
   res.cookie("token",token);
@@ -32,11 +33,84 @@ async function registerUser(req, res) {
         id:user._id,
         email:user.email,
         username:user.username
-    },
-    token
+    }
   })
 }
 
+async function loginUser(req,res){
+  const {email,username,password} = req.body;
+
+  const user = await userModel.findOne({
+    $or:[
+      {username},{email}
+    ]
+  }).select("+passwordHash");
+
+  if(!user){
+    return res.status(401).json({
+      message:"Invalid credentials",
+      status:"failed"
+    })
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if(!isPasswordCorrect){
+    return res.status(401).json({
+      message:"Invalid credentials"
+    })
+  }
+
+  const token = jwt.sign({
+    id:user._id,
+    email:user.email
+  },process.env.JWT_SECRET,{expiresIn:"1d"});
+
+  res.cookie("token",token,{
+    httpOnly:true,
+    secure:process.env.NODE_ENV === "production",
+     sameSite: "strict",    
+  maxAge: 24 * 60 * 60 * 1000
+  });
+  res.status(200).json({
+    message:"User logged in ",
+    user:{
+      username:user.username,
+      email:user.email,
+      id:user._id
+    }
+  })
+}
+
+async function getMeUser(req,res) {
+ const user = await userModel.findById(req.user.id);
+ if(!user){
+  return res.status(404).json({
+    message:"user not founded"
+  })
+ }
+  return res.status(200).json({
+    message:"user data fetched successfully",
+    user
+  })
+
+}
+
+async function logOutUser(req,res){
+  const token = req.cookies.token;
+  if(!token){
+    return res.status(401).json({
+      message:"Token not provided,unauthorizes access"
+    })
+  }
+
+  res.clearCookie("token");
+  return res.status(200).json({
+    message:"User logged out successfully"
+  })
+}
 module.exports = {
   registerUser,
+  loginUser,
+  getMeUser,
+  logOutUser
 };
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -3,7 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/auth.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 router.post("/register",authController.registerUser);
+router.post("/login",authController.loginUser);
+router.get("/get-me",authMiddleware.authUser,authController.getMeUser);
+router.get("/logout",authController.logOutUser);
 
 module.exports = router;
