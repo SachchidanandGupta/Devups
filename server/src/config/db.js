0877ede4b47bc4f96b34700a62a37cf5a3bdac2d
Sachchidanand Gupta <sachchidanandgupta2006@gmail.com>
2026-05-20 10:52:28 +0530

    DevUps intialization

---
+++ server/src/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+function connectToDB (){
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
+        console.log("Connected to database")
+    }).catch((err)=>{
+        console.log("Connection failed",err)
+    })
+}
+
+module.exports = connectToDB
