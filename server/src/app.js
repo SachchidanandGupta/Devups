6760417050782193e0bf2cd3e5af8071851797a8
Sachchidanand Gupta <sachchidanandgupta2006@gmail.com>
2026-05-26 15:01:58 +0530

    Add centralized error handling middleware for consistent error responses

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
 const friendRoutes = require("./routes/friends.routes");
+const {errorHandle} = require("./middlewares/error.middleware");
 const app = express();
 
 app.use(express.json());
@@ -11,13 +12,6 @@ app.use("/api/auth",authRoutes);
 app.use("/api/user",userRoutes);
 app.use("/api/friends",friendRoutes);
 
-app.use(function(err,req,res,next){
-    const statusCode = err.statusCode || 500 ;
-    const message = err.message || "Internal server error";
-    return res.status(statusCode).json({
-        message,
-        status:"Failed"
-    })
-})
+app.use(errorHandle);
 
 module.exports = app;
---
+++ server/src/middlewares/error.middleware.js
@@ -0,0 +1,10 @@
+async function errorHandle(err, req, res, next) {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    message,
+    status: "Failed",
+  });
+}
+
+module.exports = { errorHandle };
