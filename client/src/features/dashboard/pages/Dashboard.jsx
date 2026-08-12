eb60511ba243e3c21b4a2f99c854b2fb54fce53e
Sachchidanand Gupta <sachchidanandgupta2006@gmail.com>
2026-08-12 16:04:37 +0530

    simple redirect logic fix

--- client/src/features/dashboard/pages/Dashboard.jsx
+++ client/src/features/dashboard/pages/Dashboard.jsx
@@ -8,6 +8,7 @@ import { FaGithub } from "react-icons/fa";
 import { DashboardSkeleton } from "../../../shared/ui/Skeleton";
 import useFriend from "../../friends/hooks/useFriend";
 import NoticeBar from "../components/NoticeBar";
+import { Link } from "react-router";
 const Dashboard = () => {
   const { user, isLoading } = useAuth();
   const { fetchFriends } = useFriend();
@@ -20,7 +21,7 @@ const Dashboard = () => {
         <DashboardSkeleton />
       ) : (
         <div className="p-4 flex flex-col gap-4">
-          <NoticeBar/>
+          <NoticeBar />
           {user?.githubUsername ? (
             <div className="w-full overflow-hidden bg-black border border-border">
               {user._id && <GithubHeatmap userId={user._id} />}
@@ -34,10 +35,12 @@ const Dashboard = () => {
                 <span className="text-text-muted animate-pulse font-bold tracking-widest uppercase">
                   [NO_DATA_SIGNAL]
                 </span>
-                <button className="text-accent border flex gap-2 items-center border-accent px-5 py-2 cursor-pointer hover:bg-accent hover:text-black active:bg-danger active:text-text-primary active:border-danger transition-colors uppercase font-bold tracking-wider">
-                  <FaGithub size={16} />
-                  <span>INITIALIZE_GITHUB_NODE</span>
-                </button>
+                <Link Link to={`/profile/${user._id}`}>
+                  <button className="text-accent border flex gap-2 items-center border-accent px-5 py-2 cursor-pointer hover:bg-accent hover:text-black active:bg-danger active:text-text-primary active:border-danger transition-colors uppercase font-bold tracking-wider">
+                    <FaGithub size={16} />
+                    <span>INITIALIZE_GITHUB_NODE</span>
+                  </button>
+                </Link>
               </div>
             </div>
           )}
